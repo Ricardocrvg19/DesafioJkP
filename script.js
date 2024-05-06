d981b4d5dce57579b23289333e8f2ff6c359596f
Ricardocrvg19 <martinsricardo1927@gmail.com>
2024-05-06 18:04:01 -0300

    Game-project

---
+++ script.js
@@ -0,0 +1,88 @@
+// const result = document.querySelector("#result")
+// const ourScore = document.querySelector("#our-score")
+// const machineScore = document.querySelector("#machine-score")
+
+
+// let ourScoreNumber = 0
+// let machineScoreNumber = 0
+
+
+// const humanPlay = (humanChoice) => {
+//     timeToPlay(humanChoice, machinePlay())
+// }
+
+// const machinePlay = () => {
+//     const choices = ["rock", "paper", "scissors"]
+//     const randomNumber = Math.floor(Math.random() * 3)
+
+//     return choices[randomNumber]
+// }
+
+// const timeToPlay = (human, machine) => {
+
+//     console.log("Humano " + human + " Maquina " + machine)
+
+//     if (human === machine) {
+//         result.innerHTML = "Empatou &#x1F4A9&#x1F4A9"
+//     } else if (
+//         (human === "rock" && machine === "scissors") ||
+//         (human === "paper" && machine === "rock") ||
+//         (human === "scissors" && machine === "paper")
+//     ) {
+//         ourScoreNumber++
+//         ourScore.innerHTML = ourScoreNumber
+//         result.innerHTML = "Ganhou &#x1F601&#x1F601"
+//     } else {
+//         machineScoreNumber++
+//         machineScore.innerHTML = machineScoreNumber
+//         result.innerHTML = "Fracasso &#x1F624&#x1F624"
+//     }
+// }
+
+
+const GAME_NUMBERS = {
+    ROCK: "rock",
+    PAPER: "paper",
+    SCISSORS: "scissors"
+}
+
+const result = document.querySelector("#result")
+const ourScore = document.querySelector("#our-score")
+const machineScore = document.querySelector("#machine-score")
+
+let ourScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const humanPlay = (playHuman) => {
+
+    timeToPlay(playHuman, machinePlay())
+}
+
+const machinePlay = () => {
+    const choices = [GAME_NUMBERS.ROCK, GAME_NUMBERS.PAPER, GAME_NUMBERS.SCISSORS]
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+    
+}
+
+const timeToPlay = (human, machine) => {
+    console.log(" Humano => " + human + " maquina => " + machine)
+
+    if (human === machine) {
+        result.src = "./assets/teste-image.jpg"
+    } else if (
+        (human === GAME_NUMBERS.ROCK && machine === GAME_NUMBERS.SCISSORS) ||
+        (human === GAME_NUMBERS.PAPER && machine === GAME_NUMBERS.ROCK) ||
+        (human === GAME_NUMBERS.SCISSORS && machine === GAME_NUMBERS.PAPER)
+    ) {
+        ourScoreNumber++
+        ourScore.innerHTML = ourScoreNumber
+        result.src = "./assets/teste-image-ganhou.jpg"
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.src = "./assets/teste-image-3.jpg"
+    }
+}
